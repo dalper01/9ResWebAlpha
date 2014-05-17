430e5e0be8536553a556cbee8de9c54751adcf83
dalper01 <dave@citrusmanagement.com>
2014-05-17 19:34:59 -0400

    Add missing DataLayerFiles to DataLayer

---
+++ DataLayer/Certifications.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataLayer
+{
+    public class Certifications
+    {
+        public int Id { get; set; }
+        public string type { get; set; }
+        public string Provider { get; set; }
+
+        public bool completed { get; set; }
+        public string compMonth { get; set; }
+        public string compYear { get; set; } 
+
+    }
+}
---
+++ DataLayer/Colleges.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataLayer
+{
+    public class Colleges
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+
+        public string degreeType { get; set; }
+        public string degreeProgram { get; set; }
+
+        public bool graduated { get; set; }
+        public string gradMonth { get; set; }
+        public string gradYear { get; set; }
+
+    }
+}
