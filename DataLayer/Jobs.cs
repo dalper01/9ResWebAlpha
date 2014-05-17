0fc1823dd8c41a51e896c431af364b13ccb343c1
dalper01 <dave@citrusmanagement.com>
2014-05-17 19:34:59 -0400

    Add missing DataLayerFiles to DataLayer

---
+++ DataLayer/Jobs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataLayer
+{
+    public class Jobs
+    {
+        public int Id { get; set; }
+        public string firmLong { get; set; }  
+        public string firmShort { get; set; }
+        public string titleLong { get; set; }
+        public string titleShort { get; set; }
+
+        string hello;
+
+        public string city { get; set; }
+        public string state { get; set; }
+        public string startMonth { get; set; }
+        public string startYear { get; set; }
+
+        public virtual ICollection<JobDetails> details { get; set; }
+
+    }
+}
