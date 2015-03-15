d1a0984f7bf9ce100eb6d8cb5206da1fdaa3c0aa
dalper01 <dave@citrusmanagement.com>
2015-03-15 18:00:56 -0400

    Doc and PDF creation

--- 9ResWeb/Controllers/ResumeApiController.cs
+++ 9ResWeb/Controllers/ResumeApiController.cs
@@ -2,11 +2,20 @@
 using AutoMapper;
 using LogicLayer;
 using LogicLayer.DTOs.Resume;
+using Spire.Doc;
+using Spire.Doc.Documents;
+using Spire.Doc.Fields;
+using Spire.Pdf;
+using Spire.Pdf.Graphics;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
 using System.Web.Http;
 
 namespace _9ResWeb.Controllers
@@ -38,6 +47,208 @@ namespace _9ResWeb.Controllers
 
 
             return Request.CreateResponse(HttpStatusCode.Created, a);
+        }
+
+
+        [Route("api/getResumePDF")]
+        [AcceptVerbs("POST", "GET")]
+        public HttpResponse getResumePDF([FromBody]ResumeViewModel newResume)
+        {
+
+            PdfDocument doc = new PdfDocument();
+            PdfPageBase page = doc.Pages.Add();
+            page.Canvas.DrawString(String.Format("{0} {1} {2} sssssssssssssssssssssssssssssss", newResume.contactInfo.firstName, newResume.contactInfo.middleName, newResume.contactInfo.lastName),
+                        new PdfFont(PdfFontFamily.Helvetica, 30f),
+                        new PdfSolidBrush(Color.Navy),
+                        10, 10);
+
+            doc.SaveToHttpResponse("resume.pdf", HttpContext.Current.Response, HttpReadType.Open);
+            //response = HttpContext.Current.Response;
+
+            //FileStream to_stream = new FileStream("To_stream.pdf", FileMode.Open);
+            //doc.SaveToStream(to_stream);
+
+
+
+
+            //string path = HttpContext.Current.Server.MapPath(@"~\Content\9res-nie.css");
+            //HttpResponseMessage response = null; // = new HttpResponseMessage();
+
+            //response.Content = new StreamContent(new FileStream(filePath, FileMode.Open));
+            //response.Content = new StreamContent(to_stream);
+            //response.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment");
+            //response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/pdf");
+            //response.Content.Headers.ContentDisposition.FileName = "To_stream.pdf";
+
+            return HttpContext.Current.Response;
+        }
+
+
+        [Route("api/getResumeDOC")]
+        [AcceptVerbs("POST", "GET")]
+        public HttpResponseMessage getResumeDOC([FromBody]ResumeViewModel newResume)
+        {
+
+            //new System.IO.Stream() stream;
+            var ms = new MemoryStream();
+            //HttpResponse response = HttpContext.Current.Response;
+            HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
+
+            TextRange txtRange;
+
+            Table table;
+            TableRow CurrentRow;
+
+            Document doc = new Document();
+            Section section; // = doc.AddSection();
+            Paragraph Para; // = section.AddParagraph();
+
+            section = doc.AddSection();
+
+            Para = section.AddParagraph();
+            Para.Format.HorizontalAlignment = HorizontalAlignment.Center;
+            txtRange = Para.AppendText(String.Format("{0} {1} {2}", newResume.contactInfo.firstName, newResume.contactInfo.middleName, newResume.contactInfo.lastName));
+            txtRange.CharacterFormat.FontName = "Arial, sans-serif";
+            txtRange.CharacterFormat.FontSize = 20;
+            txtRange.CharacterFormat.Bold = true;
+            txtRange.CharacterFormat.TextColor = Color.Navy;
+
+
+            table = section.AddTable(true);
+            table.ResetCells(3, 2);
+            table.ApplyHorizontalMerge(0, 0, 1);
+
+
+            //table.TableFormat.Borders.Right.LineWidth = 0F;
+            table.TableFormat.Borders.LineWidth = 0F;
+            //table.TableFormat.Borders.Left.LineWidth = 0F;
+            //table.TableFormat.Borders.Bottom.LineWidth = 1F;
+            //table.TableFormat.Borders.Bottom.Color = Color.LightGray;
+
+            CurrentRow = table.Rows[0];
+            CurrentRow.RowFormat.Borders.Vertical.LineWidth = 0F;
+            CurrentRow.RowFormat.Borders.Horizontal.LineWidth = 0F;
+            CurrentRow.RowFormat.Borders.Top.Color = Color.LightGray;
+            //CurrentRow.RowFormat.Borders.Color = Color.LightGray;
+            CurrentRow.RowFormat.Borders.Top.LineWidth = 1F;
+            Para = CurrentRow.Cells[0].AddParagraph();
+
+            //Para = section.AddParagraph();
+            Para.Format.HorizontalAlignment = HorizontalAlignment.Center;
+            txtRange = Para.AppendText(String.Format("{0} {1} {2} {3}", newResume.contactInfo.addrStreet, newResume.contactInfo.addrTown, newResume.contactInfo.addrState, newResume.contactInfo.addrZip));
+            txtRange.CharacterFormat.FontName = "Arial, sans-serif";
+            txtRange.CharacterFormat.FontSize = 12;
+            txtRange.CharacterFormat.Bold = true;
+            txtRange.CharacterFormat.TextColor = Color.Black;
+
+
+            CurrentRow = table.Rows[1];
+            CurrentRow.RowFormat.Borders.Horizontal.Color = Color.LightGray;
+            CurrentRow.RowFormat.Borders.Color = Color.LightGray;
+            CurrentRow.RowFormat.Borders.Vertical.LineWidth = 0F;
+            CurrentRow.RowFormat.Borders.Horizontal.LineWidth = 0F;
+            CurrentRow.RowFormat.Borders.Top.Color = Color.LightGray;
+            //FRow.IsHeader = true;
+            //FRow.Cells[0].GridSpan.
+            Para = CurrentRow.Cells[0].AddParagraph();
+            Para.Format.HorizontalAlignment = HorizontalAlignment.Left;
+
+            txtRange = Para.AppendText(String.Format("{0} ", newResume.contactInfo.number1));
+            txtRange = Para.AppendText(String.Format("{0} ", newResume.contactInfo.number2));
+            txtRange.CharacterFormat.FontName = "Arial, sans-serif";
+            txtRange.CharacterFormat.FontSize = 12;
+            //txtRange.CharacterFormat.Bold = true;
+            txtRange.CharacterFormat.TextColor = Color.Black;
+            //txtRange.ApplyCharacterFormat()
+
+
+            Para = CurrentRow.Cells[1].AddParagraph();
+            //Para = section.AddParagraph();
+            txtRange = Para.AppendText(String.Format("{0} ", newResume.contactInfo.number1));
+            txtRange = Para.AppendText(String.Format("{0} ", newResume.contactInfo.number2));
+            txtRange.CharacterFormat.FontName = "Arial, sans-serif";
+            txtRange.CharacterFormat.FontSize = 12;
+            txtRange.CharacterFormat.Bold = false;
+            txtRange.CharacterFormat.TextColor = Color.Black;
+            Para.Format.HorizontalAlignment = HorizontalAlignment.Right;
+
+            CurrentRow = table.Rows[1];
+            CurrentRow.Height = 10;
+
+            foreach (var objective in newResume.objectives)
+            {
+                Para = section.AddParagraph();
+                txtRange = Para.AppendText(String.Format("{0}", objective.description));
+                txtRange.CharacterFormat.FontName = "Arial, sans-serif";
+                txtRange.CharacterFormat.FontSize = 10;
+                //txtRange.CharacterFormat.Bold = true;
+                //txtRange.CharacterFormat.TextColor = Color.Navy;
+
+
+            }
+
+            //ListStyle listStyle = new ListStyle(doc, ListType.Numbered);
+            //listStyle.Name = "levelstyle";
+            //listStyle.Levels[0].PatternType = ListPatternType.Arabic;
+            //listStyle.Levels[1].NumberPrefix = "\x0000.";
+            //listStyle.Levels[1].PatternType = ListPatternType.Arabic;
+            //listStyle.Levels[2].NumberPrefix = "\x0000.\x0001.";
+            //listStyle.Levels[2].PatternType = ListPatternType.Arabic;
+            //doc.ListStyles.Add(listStyle);
+
+
+            //table = section.AddTable(false);
+
+            TableRow TR;
+            //table.ResetCells((newResume.jobs.Count() + 1) * 2, 3);
+
+            //foreach (var job in newResume.jobs)
+            //{
+            //    TR = new TableRow(doc);
+            //    //CurrentRow = table.Rows[0];
+            //    table.AddRow(TR);
+
+            //    Para = CurrentRow.Cells[0].AddParagraph();
+            //    txtRange = Para.AppendText(String.Format("{0}", job.firmLong));
+            //    txtRange.CharacterFormat.FontName = "Arial, sans-serif";
+            //    txtRange.CharacterFormat.FontSize = 10;
+            //    txtRange.CharacterFormat.Bold = false;
+            //    txtRange.CharacterFormat.TextColor = Color.Navy;
+
+
+            //    foreach (var jobDetail in job.details)
+            //    {
+            //        Para = section.AddParagraph();
+            //        Para.AppendText("One");
+            //        Para.ListFormat.ApplyStyle("levelstyle");
+
+            //        //Para = section.AddParagraph();
+            //        //txtRange = Para.AppendText(String.Format("{0}", objective.description));
+            //        //txtRange.CharacterFormat.FontName = "Arial, sans-serif";
+            //        //txtRange.CharacterFormat.FontSize = 10;
+            //        //txtRange.CharacterFormat.Bold = true;
+            //        //txtRange.CharacterFormat.TextColor = Color.Navy;
+
+            //    }
+
+
+            //}
+
+
+
+
+            doc.SaveToStream(ms, Spire.Doc.FileFormat.Docx);
+
+
+            response.Content = new ByteArrayContent(ms.ToArray());
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/msword");
+            response.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment")
+            {
+                FileName = "Resume.docx"
+            };
+
+
+            return response;
         }
     }
 }
--- 9ResWeb/Javascript/NGControllers/ResViewController.js
+++ 9ResWeb/Javascript/NGControllers/ResViewController.js
@@ -16,86 +16,128 @@ function ($http, $scope, $rootScope, localStorageService) {
 
     $scope.SavePDF = function () {
 
-        var doc = new jsPDF('p', 'pt', 'letter');
-        doc.setFontSize(14);
-        //doc.text(1, 1, $scope.contactInfo.firstName + ' ' + $scope.contactInfo.middleName + ' ' + $scope.contactInfo.lastName + ' ');
-
-        var htmlPDF = '';
-
-        htmlPDF += '<p style="border-bottom: solid #dddddd 1px; font-size: 17px; color: red; text-align: center; width: 100%; text-decoration: underline;"> <span style=" font-family: Gabriola; color: red; font-color: red; font-weight: bold; border-bottom: solid black 1px;">';
-        htmlPDF += $scope.contactInfo.firstName + ' ' + $scope.contactInfo.middleName + ' ' + $scope.contactInfo.lastName + '</span></p> ';
-        htmlPDF += '<p style="border-bottom: solid #dddddd 1px; color: red; text-align: center; width: 100%; text-decoration: underline;"> <span style="font-size: 11px; color: red; font-color: red; font-weight: bold; border-bottom: solid black 1px;">';
-        htmlPDF += $scope.contactInfo.addrStreet + ' ' + $scope.contactInfo.addrTown + ' ' + $scope.contactInfo.addrState + ' ' + $scope.contactInfo.addrZip + '</span></p> <br>';
-        //htmlPDF += '<p style=" left: 1px; position: relative; font-size: 11px; text-color: purple; text-align: center; width: 100%">' + $scope.contactInfo.addrStreet + ' ' + $scope.contactInfo.addrTown + ' ' + $scope.contactInfo.addrState + ' ' + $scope.contactInfo.addrZip + '</p>';
-        //htmlPDF += '<span style="font-size: 11px; text-color: purple; text-align: center">' + $scope.contactInfo.addrStreet + ' ' + $scope.contactInfo.addrTown + ' ' + $scope.contactInfo.addrState + ' ' + $scope.contactInfo.addrZip + '</span>';
-
-        var index = 0;
-        for (index = 0; index < $scope.objectiveList.length; index++) {
-            htmlPDF += 'p' + $scope.objectiveList[index].description + '</p>';
-        }
-        htmlPDF += '<ul> <li>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa <br>';
-        htmlPDF += '<li> aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa </p>';
-        htmlPDF += '<li>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa <br>';
-        htmlPDF += '<li>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa </ul>';
+        var json = JSON.stringify({
+            contactInfo: localStorageService.GetContactInfo(),
+            education: localStorageService.GetEducation(),
+            jobs: localStorageService.GetJobs(),
+            skills: localStorageService.GetSkills(),
+            objectives: localStorageService.GetObjectives()
+        });
 
 
-        doc.fromHTML(htmlPDF,
-            1, 10, { width: 1000 });
-        doc.save('sample-file.pdf');
+        $http({
+            url: '/api/getResumePDF',
+            method: "POST",
+            data: json, //this is your json data string
+            headers: {
+                'Content-type': 'application/json'
+            },
+            responseType: 'arraybuffer'
+        }).success(function (data, status, headers, config) {
+            var blob = new Blob([data], { type: "application/pdf" });
+            saveAs(blob, 'sample.pdf');
+            //navigator.msSaveBlob(blob, 'test.css');
+            //var saveBlob = navigator.webkitSaveBlob || navigator.mozSaveBlob || navigator.saveBlob;
+            //saveBlob(blob, 'test.css');
+            //headers = headers();
 
 
+            //var objectUrl = URL.createObjectURL(blob);
+            //window.open(objectUrl);
+        }).error(function (data, status, headers, config) {
+            //upload failed
+        });
 
-        //var printDoc = new jsPDF();
-
-        //printDoc.fromHTML($('#prevRes').get(0), 10, 10, {
-        //    'width': 180
-        //});
-        //printDoc.autoPrint()
-        //printDoc.output("dataurlnewwindow") // this opens a new popup,  after this the PDF opens the print window view but there are browser inconsistencies with how this is handled
-
-        //printDoc.save('sample-file.pdf');
-
-        //return;
-
-
-        //  doc.fromHTML($('#prevRes').html(), 15, 15, {
-        //      'width': 1000,
-        //      'elementHandlers': {
-        //          'H1': (el, renderer) =>
-        //doc.setFontSize(14)
-        //  doc.setFontStyle('bold')
-        //  doc.text($(el).text(), 10, renderer.y)
-        //  doc.setFontSize(12)
-        //  doc.setFontStyle('normal')
-        //  true}
-        //  });
-        //  doc.save('sample-file.pdf');
-
-        //var source = $("#prevRes").innerHTML();
-        //console.log(source);
 
         return;
 
-        doc.fromHTML(source, 10, 10, {
-            'width': 170,
-            'elementHandlers': elementHandler
-            //          'LI': (el, renderer) =>
-            //            if renderer.y > 250
-            //    doc.addPage()
-            //    renderer.y = 10
-            //else
-            //    renderer.y += 10
-            //    false
-            //    'H1': (el, renderer) =>
-            //      doc.setFontSize(14)
-            //    doc.setFontStyle('bold')
-            //    doc.text($(el).text(), 10, renderer.y)
-            //    doc.setFontSize(12)
-            //    doc.setFontStyle('normal')
-            //    true
-            //    doc.output 'dataurlnewwindow', {}
+        var docDefinition = {
+            content: [
+
+                {
+                    text: $scope.contactInfo.firstName + ' ' + $scope.contactInfo.middleName + ' ' + $scope.contactInfo.lastName + ' ',
+                    fontSize: 15,
+                    bold: true,
+                    alignment: 'center',
+                    color: "#285EA6"
+                }
+
+            ]
+        };
+        pdfMake.createPdf(docDefinition).open();
+
+
+        //var doc = new jsPDF('p', 'pt', 'letter');
+        //doc.setFontSize(14);
+        //doc.text(1, 1, $scope.contactInfo.firstName + ' ' + $scope.contactInfo.middleName + ' ' + $scope.contactInfo.lastName + ' ');
+
+        //var htmlPDF = '';
+
+        //htmlPDF += '<p style="border-bottom: solid #dddddd 1px; font-size: 17px; color: red; text-align: center; width: 100%; text-decoration: underline;"> <span style=" font-family: Gabriola; color: red; font-color: red; font-weight: bold; border-bottom: solid black 1px;">';
+        //htmlPDF += $scope.contactInfo.firstName + ' ' + $scope.contactInfo.middleName + ' ' + $scope.contactInfo.lastName + '</span></p> ';
+        //htmlPDF += '<p style="border-bottom: solid #dddddd 1px; color: red; text-align: center; width: 100%; text-decoration: underline;"> <span style="font-size: 11px; color: red; font-color: red; font-weight: bold; border-bottom: solid black 1px;">';
+        //htmlPDF += $scope.contactInfo.addrStreet + ' ' + $scope.contactInfo.addrTown + ' ' + $scope.contactInfo.addrState + ' ' + $scope.contactInfo.addrZip + '</span></p> <br>';
+        //htmlPDF += '<p style=" left: 1px; position: relative; font-size: 11px; text-color: purple; text-align: center; width: 100%">' + $scope.contactInfo.addrStreet + ' ' + $scope.contactInfo.addrTown + ' ' + $scope.contactInfo.addrState + ' ' + $scope.contactInfo.addrZip + '</p>';
+        //htmlPDF += '<span style="font-size: 11px; text-color: purple; text-align: center">' + $scope.contactInfo.addrStreet + ' ' + $scope.contactInfo.addrTown + ' ' + $scope.contactInfo.addrState + ' ' + $scope.contactInfo.addrZip + '</span>';
+
+        //var index = 0;
+        //for (index = 0; index < $scope.objectiveList.length; index++) {
+        //    htmlPDF += 'p' + $scope.objectiveList[index].description + '</p>';
+        //}
+        //htmlPDF += '<ul> <li>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa <br>';
+        //htmlPDF += '<li> aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa </p>';
+        //htmlPDF += '<li>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa <br>';
+        //htmlPDF += '<li>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa </ul>';
+
+
+        //doc.fromHTML(htmlPDF,
+        //    1, 10, { width: 1000 });
+        //doc.save('sample-file.pdf');
+
+
+
+        return;
+
+        //doc.fromHTML(source, 10, 10, {
+        //    'width': 170,
+        //    'elementHandlers': elementHandler
+        //});
+
+    };
+
+    $scope.SaveDOC = function () {
+
+        var json = JSON.stringify({
+            contactInfo: localStorageService.GetContactInfo(),
+            education: localStorageService.GetEducation(),
+            jobs: localStorageService.GetJobs(),
+            skills: localStorageService.GetSkills(),
+            objectives: localStorageService.GetObjectives()
         });
 
+
+        $http({
+            url: '/api/getResumeDOC',
+            method: "POST",
+            data: json, //this is your json data string
+            headers: {
+                'Content-type': 'application/json'
+            },
+            responseType: 'arraybuffer'
+        }).success(function (data, status, headers, config) {
+            var blob = new Blob([data], { type: "application/msword" });
+            saveAs(blob, 'Resume.docx');
+            //navigator.msSaveBlob(blob, 'test.css');
+            //var saveBlob = navigator.webkitSaveBlob || navigator.mozSaveBlob || navigator.saveBlob;
+            //saveBlob(blob, 'test.css');
+            //headers = headers();
+
+
+            //var objectUrl = URL.createObjectURL(blob);
+            //window.open(objectUrl);
+        }).error(function (data, status, headers, config) {
+            //upload failed
+        });
     };
 
 });
